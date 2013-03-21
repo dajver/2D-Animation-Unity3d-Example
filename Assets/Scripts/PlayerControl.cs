8e77be361dc8b7ab7bdad451d79c99807e8177bb
dajver4 <lfqdth5@gmail.com>
2013-03-21 17:39:50 +0300

    Simple working with sprite animation in unity3d

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+/**
+ * Play animation with keboard buttons. 
+ * */
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	void Update() {
+		AnimeScript animation = GetComponent<AnimeScript>();
+		// right
+		if(Input.GetKey("d")) {
+			animation.aniSprite(3,4,0,2,2,10);
+		}
+		//left
+		if(Input.GetKey("a")){
+			animation.aniSprite(3,4,0,1,2,10);
+		}
+		//up
+		if(Input.GetKey("w")) {
+			animation.aniSprite(3,4,0,3,2,10);	
+		}
+		//down
+		if(Input.GetKey("s")) {
+			animation.aniSprite(3,4,0,4,2,10);	
+		}
+	}
+}
